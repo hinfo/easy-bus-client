f61dc863bc981d981ea56b87bbfad9b6cae4a5c9
Henrique Antunes <hinfo@gmx.com>
2017-10-28 12:14:54 -0200

    Adicionado acesso ao webservice

--- app/src/main/java/unifra/edu/mobile/easybus/Principal.java
+++ app/src/main/java/unifra/edu/mobile/easybus/Principal.java
@@ -1,7 +1,16 @@
 package unifra.edu.mobile.easybus;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +18,56 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        String url = "http://easy-bus.herokuapp.com";
+
+        new Acessa().execute(url);
     }
+
+
+    public class Acessa extends AsyncTask<String, String,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String resposta = "";
+            HttpURLConnection urlConnection = null;
+            BufferedReader br = null;
+
+            try {
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream in = urlConnection.getInputStream();
+                br = new BufferedReader(new InputStreamReader(in));
+
+                String inputLine;
+                StringBuffer buffer = new StringBuffer();
+
+                while ((inputLine = br.readLine()) != null) {
+                    resposta = inputLine;
+                    buffer.append(resposta);
+                }
+                br.close();
+                System.out.println(buffer);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                resposta = "Erro ao acessar a URL";
+            } catch (IOException e) {
+                e.printStackTrace();
+                resposta = "Não foi possível receber os dados";
+            } finally {
+                urlConnection.disconnect();
+            }
+
+
+            return resposta;
+        }
+
+        @Override
+        protected void onPostExecute(String ip) {
+            super.onPostExecute(ip);
+//            textView.setText(ip.toString());
+        }
+    }
+
+
 }
