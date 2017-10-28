a4ce9c493c39652d81b1c8960498febb5bf1f7d6
Henrique Antunes <hinfo@gmx.com>
2017-10-28 12:22:41 -0200

    Ajustes

--- app/src/main/java/unifra/edu/mobile/easybus/Principal.java
+++ app/src/main/java/unifra/edu/mobile/easybus/Principal.java
@@ -1,21 +1,8 @@
 package unifra.edu.mobile.easybus;
 
 import android.os.AsyncTask;
-<<<<<<< HEAD
-=======
-import android.support.v7.app.AppCompatActivity;
->>>>>>> teste
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -82,7 +69,6 @@ public class Principal extends AppCompatActivity {
         }
     }
 
-<<<<<<< HEAD
     public void checkVersion() throws MalformedURLException {
         String url = "https://easy-bus.herokuapp.com/";
         new Acessa().execute(url);
@@ -90,56 +76,5 @@ public class Principal extends AppCompatActivity {
     }
 
 
-    public class Acessa extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            String resposta = "";
-            HttpURLConnection urlConnection = null;
-            BufferedReader br = null;
-
-            try {
-                URL url = new URL(params[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.connect();
-                InputStream in = urlConnection.getInputStream();
-                br = new BufferedReader(new InputStreamReader(in));
-
-                String inputLine;
-                StringBuffer buffer = new StringBuffer();
-
-                while ((inputLine = br.readLine()) != null) {
-                    resposta = inputLine;
-                    buffer.append(resposta);
-                }
-                br.close();
-                System.out.println(buffer);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                resposta = "Erro ao acessar a URL";
-            } catch (IOException e) {
-                e.printStackTrace();
-                resposta = "Não foi possível receber os dados";
-            } finally {
-                urlConnection.disconnect();
-            }
-
-
-            return resposta;
-
-
-        }
-
-        @Override
-        protected void onPostExecute(String ip) {
-            super.onPostExecute(ip);
-//            textView.setText(ip.toString());
-            Log.d("IP", "Mostrnado o resultado " + ip);
-            System.out.println("Mostrnado o resultado " + ip);
-        }
-    }
-
-=======
->>>>>>> teste
 
 }
