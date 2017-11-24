65e21c0754a3ed64a91cb986f9e649a595103cb0
hinfo <hinfo@gmx.com>
2017-11-24 10:31:32 -0200

    Adicionado Splash

---
+++ app/src/main/java/unifra/edu/mobile/easybus/Splash.java
@@ -0,0 +1,26 @@
+package unifra.edu.mobile.easybus;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,Principal.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
